4215b255e9900182122c9f06a42d66fa8dc42865
anhtruong <phanhpompap@gmail.com>
2023-09-21 11:36:56 +0200

    video player simple

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoPlayer from "./VideoPlayer";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/VideoPlayer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { DefaultPlayer as Video } from "react-html5video";
+import "react-html5video/dist/styles.css";
+import introVideo from "./assets/image/video-britianGotTalent.mp4";
+import thum from "./assets/image/britain-god-talent.jpg";
+import "./App.css";
+
+function VideoPlayer() {
+  const videoStyle = {
+    width: "750px",
+    height: "500px",
+    display: "flex",
+    justifyContent: "center",
+    marginInline: "auto",
+  };
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100vw",
+        }}
+      >
+        <h1>Britian God Talent</h1>
+        <Video
+          autoPlay
+          loop
+          poster={thum}
+          style={videoStyle}
+          className="video-frame"
+        >
+          <source src={introVideo} type="video/webm" />
+        </Video>
+      </div>
+    </>
+  );
+}
+
+export default VideoPlayer;
